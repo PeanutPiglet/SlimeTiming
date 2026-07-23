ed388e019dbd8b0f5fb399cbae81c96e2364e6f9
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-23 15:12:28 -0300

    feat: add custom call during tower death

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -2,11 +2,16 @@
 extends Node
 
 @export var health = 1
-
+@export var destroy_on_death = true
 
 @abstract func activate()
 	
 func take_damage(d:int):
 	health -= d
 	if health <= 0:
-		queue_free()
+		on_death()
+		if destroy_on_death:
+			queue_free()
+
+func on_death():
+	pass
