fcb2b3dccb7a900be167b5e410c62d7973496b65
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-23 14:18:23 -0300

    feat: update explode_troop to deal damage

--- scripts/explode_troop.gd
+++ scripts/explode_troop.gd
@@ -1,6 +1,8 @@
 class_name Explode_troop
 extends Troop
 
+@export var damage = 1
+
 func _ready():
 	super._ready()
 	
@@ -9,7 +11,7 @@ func activate():
 	explosion.monitoring = true
 	print(explosion.get_overlapping_areas())
 	for body in explosion.get_overlapping_areas():
-		var tower = body.get_parent()
-		if tower.is_in_group("tower_damageable"):
-			tower.queue_free()
+		var tower = body.get_parent() as Tower
+		if tower and tower.is_in_group("tower_damageable"):
+			tower.take_damage(damage)
 	queue_free()
