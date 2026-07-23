a429897af787bf38134e23173730cf2e85d37cc0
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-23 16:36:18 -0300

    feat: add movement to towerprojectile

--- scripts/towerprojectile.gd
+++ scripts/towerprojectile.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@export var speed = 400
+@export var duration = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var timer = Timer.new()
+	timer.wait_time = duration
+	timer.one_shot = true
+	timer.autostart = true
+	timer.timeout.connect(queue_free)
+	add_child(timer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,3 +27,9 @@ func activate():	#timer
 	
 func deactivate():
 	$AnimatedSprite2D.pause()
+	
+
+func _physics_process(delta: float) -> void:
+	var direction = -transform.y
+	position += direction * speed * delta
+	
