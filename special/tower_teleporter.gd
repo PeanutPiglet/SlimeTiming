49d1aead5fa1afe600c2722e7622cf211747148b
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-24 17:04:37 -0300

    feat: add teleporter

---
+++ special/tower_teleporter.gd
@@ -0,0 +1,30 @@
+class_name TowerTeleporter extends Tower
+
+@export var destination: TowerTeleporter
+@export var tower: Tower
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+func activate():
+	print("TELEPORTING")
+	if tower:
+		$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	if not destination or not tower:
+		return
+	if destination.tower:
+		return
+		
+	tower.global_position = destination.global_position
+	destination.tower = tower
+	tower = null
