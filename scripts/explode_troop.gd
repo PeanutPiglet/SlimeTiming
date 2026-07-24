fd4b878295526816ae4942db545e8068eac31590
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-24 11:23:33 -0300

    fix: recover proper explode_troop hit tracer

--- scripts/explode_troop.gd
+++ scripts/explode_troop.gd
@@ -9,9 +9,8 @@ func activate():
 	var explosion = $SquareExplosion
 	explosion.monitoring = true
 	print(explosion.get_overlapping_areas())
-	for body in explosion.get_overlapping_areas():
-		var tower = body.get_parent()
-		print(tower)
-		if tower.is_in_group("tower_damageable"):
-			tower.queue_free()
+	for area in explosion.get_overlapping_areas():
+		var tower = area.get_parent() as Tower
+		if tower and area.is_in_group("tower_damageable"):
+			tower.take_damage(damage)
 	queue_free()
