c3e6f29e861fe53ea5816d3f015af9b741ead2bc
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-25 14:52:41 -0300

    fix: update trooper code

--- scripts/booster_troop.gd
+++ scripts/booster_troop.gd
@@ -6,7 +6,7 @@ func _ready():
 	
 func activate():
 	
-	var troops = get_tree().current_scene.get_troops()
+	var troops = get_tree().current_scene.active_level.get_troops()
 	var grid_pos = grid.local_to_map(position)
 	var targets = GridHelper.three_by_three(grid_pos) + [grid_pos]
 	for t:Troop in troops:
